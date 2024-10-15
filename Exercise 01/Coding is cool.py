03fbfd7a08f70d8f99874bd05cc126424d922d08
Gabriel-Bucay <gbz082007@gmail.com>
2024-10-15 13:06:10 +0400

    Coding is cool

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,8 @@
+#Code exercise 1 
+    #Each variable is designated a string 
+word1 = "Coding "
+word2 = "Is "
+word3 = "Cool"
+
+#Variables are connected via string concantenation in one print statement
+print (word1 + word2 + word3)
