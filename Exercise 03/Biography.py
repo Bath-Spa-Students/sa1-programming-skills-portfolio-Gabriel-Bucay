311724c4411223c9006cff980cabe639c8e1f214
Gabriel-Bucay <gbz082007@gmail.com>
2024-10-28 10:32:07 +0400

    Biography/ advanced Biography

--- Exercise 03/Biography.py
+++ Exercise 03/Biography.py
@@ -5,20 +5,14 @@
 2. Print the values on separate lines using a single `print()` statement.
 3. Use variables with appropriate data types for each piece of information.
 
-### Advanced Requirements:
-Have the user input their name, hometown, and age instead of hardcoding the values.
-Try giving both your first and second name when asked for your name. What happens? How can you handle multiple words in Python?
-Test the program by entering a string value for age (e.g., "twenty"). What happens? How can you prevent this issue?
 '''
 #declaring variables
-Uno_Name = str(input("Enter First Name:"))
-Dos_Name = str(input("Enter Second Name:"))
+Name = str(input("Enter First Name:"))
 Hometown = str(input("Enter Hometown:"))
 Age = int(input("Enter Age:")) 
 
 #displaying questions using print statement
 
-print(  "Please Enter First Name:" + Uno_Name + '\n' + 
-        "Please Enter Second Name:" + Dos_Name + '\n' +
+print(  "Please Enter First Name:" + Name + '\n' + 
         "Please Enter Age:" + str(Age) + '\n' + 
         "Please Enter Hometown:" + Hometown + '\n')
