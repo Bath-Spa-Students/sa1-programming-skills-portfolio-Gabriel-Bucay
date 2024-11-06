66febd235e5a47e08c503971ddcb80c36f4f85cf
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-06 12:51:05 +0400

    Exercise 10 is it even?

---
+++ Exercise 10/Is it even.py
@@ -0,0 +1,42 @@
+'''
+Write a program that tests if a value is even or odd. Follow the instructions outlined below:
+
+### Instructions:
+* The program should ask the user for a number from within the main function.
+* The entered number should be passed to a function that determines if the value is even or odd.
+* The function should return a message indicating whether the number is even or odd.
+* The message returned by the function should be printed from within the main function.
+'''
+
+def is_it_even(number):
+    #if else statement to compare for even or odd
+    if number %2 == 0:
+        return f"This number {number} is even"
+    else:
+        return f"This number {number} is odd"
+    
+#Define funtion for user input
+def main():
+    number = int(input("Please Input a Number: "))
+    answer = is_it_even(number)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
+    
+
+
+
+
+
+
+
+num = int(input("Please Enter a Number: "))
+if(num % 2) == 0:
+    print("{0}is even".format(num))
+else:
+    print("{0} is odd".format(num))
