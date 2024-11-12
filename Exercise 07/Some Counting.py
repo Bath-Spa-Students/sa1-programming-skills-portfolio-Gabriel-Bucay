51dc55d5de8b4c5b56d1496ad4509efecea1e9c7
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-12 09:12:02 +0400

    Exercise 07 Some Counting

--- Exercise 07/Some Counting.py
+++ Exercise 07/Some Counting.py
@@ -8,19 +8,23 @@ in each case.
 * Write a loop that counts up from 100 to 200 in increments of 5.
 *You may include all loops in a single project*
 '''
-
-print("0 to 50 by 1")
 #using for loops to count 
+print("0 to 50 by 1")
 for n in range(0, 51, 1):
     print(n)
+
 print("50 to 0 by 1")
 for x in range(50,-1,-1):
     print(x)
+
 print("30 to 50 by 1")
 for m in range(30,51,1):
     print(m)
+
+print("50 to 10 by 2")
 for y in range(50,8,-2):
     print(y)
-print("100 to 200")
+    
+print("100 to 200 by 5")
 for t in range(100,201,5):
   print(t)
