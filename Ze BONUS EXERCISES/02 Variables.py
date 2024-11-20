505f3aa280a2a2b98634022d36c83c626793513b
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-20 22:21:20 +0400

    BE 02 Variables

---
+++ Ze BONUS EXERCISES/02 Variables.py
@@ -0,0 +1,12 @@
+'''
+Variables:
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.
+'''
+Secret = "Bober-kun"
+
+print(Secret)
+
+Secret = "Linganguli Guli Guli Guacha"
+
+print(Secret)
