4a0c870695ff7802f661ffbe989d3faacd03b9c4
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-20 22:21:49 +0400

    BE 03 Variables (Fav num)

---
+++ Ze BONUS EXERCISES/03 Variables(Favorite Numbers).py
@@ -0,0 +1,11 @@
+'''
+Variable(Favorite Numbers):
+Use a variable to represent your favorite number. Then, using that variable, create a
+message that reveals your favorite number. Print that message.
+'''
+#setting variables
+fav_numbers = 22
+note = f"my favorite numbers is {fav_numbers}" #use f-string for allow embedding the value of the fav-numbers
+
+#print output
+print(note)
