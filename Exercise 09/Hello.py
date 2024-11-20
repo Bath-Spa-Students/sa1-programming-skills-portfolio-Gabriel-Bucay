548e9d115e6533fd803e331e24c8ff62b8b39fd9
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-20 21:40:58 +0400

    Exercise 09 Hello

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -4,7 +4,7 @@ Fill in the blanks in the code below so that the function hello() prints "Hello"
 '''
 
 def hello():
-    print("hello")  # Fill in this blank to print "Hello" to the console
+    print("Hello")  # Fill in this blank to print "Hello" to the console
 
 def main():
     hello() # Fill in this blank to call the hello() function
