0847508ecd8662ccff915deca944f35fa89e12c6
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-20 22:21:11 +0400

    BE 01 string concantenation

---
+++ Ze BONUS EXERCISES/01 String Concantenation.py
@@ -0,0 +1,13 @@
+'''
+String Concantenation
+Write three strings in different variables and print the output as one string.
+'''
+#assigning strings
+prop1 = "My name"
+prop2 = " is"
+prop3 = " JEFF"
+
+#printing output
+final = prop1 + prop2 + prop3
+
+print(final)
