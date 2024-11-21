04b24cd7cbafb87447ebd3e81a35e6b83d00ad0f
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-21 17:47:10 +0400

    BE 06 Dictionaries

---
+++ Ze BONUS EXERCISES/06 Dictionaries.py
@@ -0,0 +1,21 @@
+'''
+Dictionaries:
+Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.
+'''
+#Creating dictionary
+Target_info = {
+    'First_name': 'Destiny',
+    'Last_name': 'Holmes',
+    'Age': 18,
+    'City': 'Bradford'
+}
+
+#print each part of info stored
+print("First name:", Target_info['First_name'])
+print("Last name:", Target_info['Last_name'])
+print("Age:", Target_info['Age'])
+print("City:", Target_info['City'])
