9bef711269178ac29341bfba82f82ca606b88fa0
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-21 17:46:40 +0400

    BE 04 List-Names

---
+++ Ze BONUS EXERCISES/04 List-Names.py
@@ -0,0 +1,11 @@
+'''
+List-Names:
+Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.
+'''
+#create list
+friends_list = ["Janna", "Mary", "Alice", "John", "Gael"]
+
+#printing output using for and in to access each name one by one
+for name in friends_list:
+    print(name)
