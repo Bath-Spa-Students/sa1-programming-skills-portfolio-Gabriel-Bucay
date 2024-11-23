6f7cc58288a22e43f49786bdbc72edb0a6ddaf20
Gabriel-Bucay <gbz082007@gmail.com>
2024-11-23 12:52:52 +0400

    BE 08 Function Favorite books

---
+++ Ze BONUS EXERCISES/08 Functions Favorite books.py
@@ -0,0 +1,11 @@
+'''
+Functions faorite books:
+Write a function called favorite_book() that accepts one parameter, title. The function
+should print a message, such as One of my favorite books is Alice in Wonderland. Call the function, making sure to include a book
+title as an argument in the function call.
+'''
+#Creating the book funciton
+def favorite_book(Title):
+    print(f"My favorite book is {Title}")
+
+favorite_book("Alice in Wonderland")
